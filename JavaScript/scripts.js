f58edf9850208d0e60806d53f8b6e1101aecde62
Charis Giannopoulos <charilaos.giannopoulos@acg.edu>
2024-03-08 14:56:28 +0200

    Added additional carousel slides, Made the navbar fixed to the top of the page, and added padding to accomodate the page's content.

---
+++ JavaScript/scripts.js
@@ -0,0 +1,13 @@
+/*
+$(document).ready(function() {
+    $('#contact-form').submit(function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Get form data
+        var formData = $(this).serialize();
+
+        // Display submitted data as a pop-up
+        alert('Form submitted!\n' + formData);
+    });
+});
+*/
